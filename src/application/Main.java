24e1641e95fa17c7e6b0b060ca654301a4bfc0c4
paschoalAr <arthur.paschoal@edu.pucrs.br>
2024-08-20 09:51:44 -0300

    Adicionar os dados no arquivo na lista de empregados

--- src/application/Main.java
+++ src/application/Main.java
@@ -6,20 +6,24 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
 
+import entities.Employee;
+
 public class Main {
 
     public static void main(String[] args) throws FileNotFoundException {
-    
+        
+        List<Employee> employees = new ArrayList<>();
+
         String line = null;
         Scanner sc = new Scanner(new File("employee.txt"));
         while (sc.hasNextLine()) {
             line = sc.nextLine();
-            System.out.println(line);
-            // String nomeProduto = line.split(",")[0];
-            // Double precoProduto = Double.valueOf(line.split(",")[1]).doubleValue();
-
-            
-            
+            String nameEmployee = line.split(",")[0];
+            String emailEmployee = line.split(",")[1];
+            Double salaryEmployee = Double.valueOf(line.split(",")[2]).doubleValue();
+            employees.add(new Employee(nameEmployee, emailEmployee, salaryEmployee));
         }
+
+        employees.forEach(System.out:: println);
     }
 }
